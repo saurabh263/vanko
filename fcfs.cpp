ceb7ca745f0fdf014c5ff146af6b45bf95b6ef3a
Saurabh_263 <95668982+saurabh263@users.noreply.github.com>
2023-11-21 06:29:28 +0530

    Update fcfs.cpp

--- fcfs.cpp
+++ fcfs.cpp
@@ -1,64 +1,27 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
-void findwaitingtime(int processes[], int n, int bt[], int wt[], int at[])
-{
-    wt[0] = 0;
-   
-    for (int i = 1; i < n; i++)
-    {
-        wt[i] = bt[i - 1] + wt[i - 1] - at[i];
-        // Ensure waiting time is not negative
-        if (wt[i] < 0) {
-            wt[i] = 0;
+int main(){
+    int n;cin>>n;
+    vector<pair<pair<int,int>,int>>v;
+    for(int i=0;i<n;i++){
+        int x,y;cin>>x>>y;
+        v.push_back({{x,y},i});
+    }
+    sort(v.begin(),v.end());
+    int a[n]={0};
+    int x=v[0].first.first;
+    for(int i=0;i<n;i++){
+        if(x<=v[i].first.first){
+            a[v[i].second]=(v[i].first.first + v[i].first.second);
+            x=a[v[i].second];
+        }
+        else{
+            a[v[i].second]=(v[i].first.first + v[i].first.second + (abs(x-(v[i].first.first))));
+            x=a[v[i].second];
         }
     }
-}
-
-void findturnaroundtime(int processes[], int n, int bt[], int wt[], int tat[])
-{
-    for (int i = 0; i < n; i++)
-    {
-        tat[i] = bt[i] + wt[i];
-    }
-}
-
-void findavgtime(int processes[], int n, int bt[], int at[])
-{
-    int wt[n], tat[n];
-    int total_wt = 0;
-    int total_tat = 0;
-
-    findwaitingtime(processes, n, bt, wt, at);
-
-    findturnaroundtime(processes, n, bt, wt, tat);
-  
-    cout << "processes  "
-         << "arrival_time  "
-         << "burst_time  "
-         << "Waiting_time "
-         << " turn_around_time\n"<<endl;
-
-    for (int i = 0; i < n; i++)
-    {
-        total_wt = total_wt + wt[i];
-        total_tat = total_tat + tat[i];
-        cout << "   " << i+1 << "\t\t" << at[i] << "\t\t" << bt[i] <<"\t    "
-            << wt[i] <<"\t\t  " << tat[i] <<endl;
-        
-    }
-    cout << "Average waiting time = "
-         << (float)total_wt / (float)n;
-    cout << "\nAverage turn around time = "
-         << (float)total_tat / (float)n;
-}
-
-int main()
-{
-    int processes[] = {1, 2, 3};
-    int burst_t[] = {10, 5, 8};
-    int arrival_t[] = {0, 2, 4}; // Example arrival times
-    int n = sizeof(processes) / sizeof(processes[0]);
-    findavgtime(processes, n, burst_t, arrival_t);
-    return 0;
+    cout<<"processno "<< " arrival "<< " brust "<<" compliton "<<" waiting time "<<" tot"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<i+1<<" "<<v[i].first.first<<" "<<v[i].first.second<<" "<<a[i]<<" "<<a[i]-((v[i].first.first + v[i].first.second))<<" "<<a[i]-(v[i].first.first)<<endl;
+    } 
 }
